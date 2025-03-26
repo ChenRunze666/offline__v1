ac559c0c2eb5266e91dbab08affd823554b38b2b
BeiGu777 <1725509470@qq.com>
2025-03-26 21:26:36 +0800

    add 0325 file

---
+++ sql/hive3.sql
@@ -0,0 +1,26 @@
+insert overwrite table dwd_display_log partition (dt = '2025-03-22')
+select
+    get_json_object(line,'$.common.ar'),
+    get_json_object(line,'$.common.ba'),
+    get_json_object(line,'$.common.ch'),
+    get_json_object(line,'$.common.is_new'),
+    get_json_object(line,'$.common.md'),
+    get_json_object(line,'$.common.mid'),
+    get_json_object(line,'$.common.os'),
+    get_json_object(line,'$.common.uid'),
+    get_json_object(line,'$.common.vc'),
+    get_json_object(line,'$.page.during_time'),
+    get_json_object(line,'$.page.item'),
+    get_json_object(line,'$.page.item_type'),
+    get_json_object(line,'$.page.last_page_id'),
+    get_json_object(line,'$.page.page_id'),
+    get_json_object(line,'$.page.source_type'),
+    get_json_object(line,'$.ts'),
+    get_json_object(display,'$.display_type'),
+    get_json_object(display,'$.item'),
+    get_json_object(display,'$.item_type'),
+    get_json_object(display,'$.order'),
+    get_json_object(display,'$.pos_id')
+from ods_log lateral view
+    explode_json_array(get_json_object(line,'$.display')) tmp as display
+where dt='2025-03-22' and get_json_object(line,'$.display') is not null ;
